76615539aa04b1a8cf89626949c119c9b4fe975a
James Westhead <james_westhead@outlook.com>
2023-05-24 21:58:22 +0100

    added function and db record

--- App/static/scripts/recordSession.js
+++ App/static/scripts/recordSession.js
@@ -9,175 +9,81 @@ let friday = [["Leg Press (Glutes)",3],["RDLs",2],["Leg Extensions",3],["Leg Pre
 
 function addExerciseToPage(form,exercise,sets){
     let container = document.createElement("div")
-    
+    container.setAttribute("id",`${exercise.replace(" ","-")}-container`)
+    container.setAttribute("class","exercise-container")
+    form.appendChild(container)
+
+    let exerciseContainer = document.getElementById(`${exercise.replace(" ","-")}-container`)
+        let h3 = document.createElement("h3")
+        h3.innerText = exercise
+        exerciseContainer.appendChild(h3)
+
+    for(i=0;i<sets;i++){
+        let setContainer = document.createElement("div")
+        setContainer.setAttribute("id",`${exercise.replace(" ","-")}-set${i+1}-container`)
+        setContainer.setAttribute("class","set-container")
+        exerciseContainer.appendChild(setContainer)
+
+        setContainer = document.getElementById(`${exercise.replace(" ","-")}-set${i+1}-container`)
+
+        let label = document.createElement("label")
+        label.innerText = "Weight:"
+        label.setAttribute("for",`${exercise.replace(" ","-")}-set${i+1}-weight`)
+        setContainer.appendChild(label)
+
+        let weightInput = document.createElement("input")
+        weightInput.setAttribute("type","number")
+        weightInput.setAttribute("step","0.01")
+        weightInput.setAttribute("id",`${exercise.replace(" ","-")}-set${i+1}-weight`)
+        weightInput.setAttribute("name",`${exercise.replace(" ","-")}-set${i+1}-weight`)
+        setContainer.appendChild(weightInput)
+
+        label = document.createElement("label")
+        label.innerText = "Reps:"
+        label.setAttribute("for",`${exercise.replace(" ","-")}-set${i+1}-reps`)
+        setContainer.appendChild(label)
+
+        let repsInput = document.createElement("input")
+        repsInput.setAttribute("type","number")
+        repsInput.setAttribute("step","1")
+        repsInput.setAttribute("id",`${exercise.replace(" ","-")}-set${i+1}-reps`)
+        repsInput.setAttribute("name",`${exercise.replace(" ","-")}-set${i+1}-reps`)
+        setContainer.appendChild(repsInput)
+
+    }
+}
+
+function addSubmitButton(form){
+    let submitButton = document.createElement("button")
+    submitButton.innerText = "Submit"
+    submitButton.setAttribute("type","submit")
+    submitButton.setAttribute("value","Submit")
+    form.appendChild(submitButton)
+
 }
 
 window.onload = () =>{
     const form = document.getElementById("record-form")
 
     if(day === 1){
-        monday.forEach(item=>{
-            console.log(item)
-        })
-        // let container = document.createElement("div")
-        // container.setAttribute("id","smith-hack-squat-container")
-        // form.appendChild(container)
-        //
-        // let smithHackSmithContainer = document.getElementById("smith-hack-squat-container")
-        // let h3 = document.createElement("h3")
-        // h3.innerText = "Smith Hack Squat"
-        // smithHackSmithContainer.appendChild(h3)
+        monday.forEach(item=>addExerciseToPage(form,item[0],item[1]))
+        addSubmitButton(form)
 
     }
     else if(day===2){
-        tuesday.forEach(item=>{
-            addExerciseToPage(form,item[0],item[1])
-        })
-        // let container = document.createElement("div")
-        // container.setAttribute("id","pec-deck-container")
-        // form.appendChild(container)
-        //
-        // let smithHackSmithContainer = document.getElementById("pec-deck-container")
-        // let h3 = document.createElement("h3")
-        // h3.innerText = "Pec Deck"
-        // smithHackSmithContainer.appendChild(h3)
+        tuesday.forEach(item=> addExerciseToPage(form,item[0],item[1]))
+        addSubmitButton(form)
     }
     else if(day===3){
-        let container = document.createElement("div")
-        container.setAttribute("id","laying-hamstring-curls-container")
-        form.appendChild(container)
-
-        let smithHackSmithContainer = document.getElementById("laying-hamstring-curls-container")
-        let h3 = document.createElement("h3")
-        h3.innerText = "Laying Hamstring Curls"
-        smithHackSmithContainer.appendChild(h3)
+        wednesday.forEach(item=> addExerciseToPage(form,item[0],item[1]))
+        addSubmitButton(form)
     }
-    // Thursday
     else if(day===4){
-        let container = document.createElement("div")
-        container.setAttribute("id","smith-shoulder-press-container")
-        form.appendChild(container)
-
-        // Smith Shoulder Press
-        let smithShoulderPressContainer = document.getElementById("smith-shoulder-press-container")
-        let h3 = document.createElement("h3")
-        h3.innerText = "Smith Shoulder Press"
-        smithShoulderPressContainer.appendChild(h3)
-
-        // set 1
-        container = document.createElement("div")
-        container.setAttribute("id","smith-shoulder-press-set1-container")
-        smithShoulderPressContainer.appendChild(container)
-
-        let smithShoulderPressContainerSet1 = document.getElementById("smith-shoulder-press-set1-container")
-        let h4 = document.createElement("h4")
-        h4.innerText = "Set 1"
-        smithShoulderPressContainerSet1.appendChild(h4)
-
-        let label = document.createElement("label")
-        label.innerText = "Weight:"
-        label.setAttribute("for","smith-shoulder-press-set1-weight")
-        smithShoulderPressContainerSet1.appendChild(label)
-
-        let weightInput = document.createElement("input")
-        weightInput.setAttribute("type","number")
-        weightInput.setAttribute("step","0.01")
-        weightInput.setAttribute("id","smith-shoulder-press-set1-weight")
-        weightInput.setAttribute("name","smith-shoulder-press-set1-weight")
-        smithShoulderPressContainerSet1.appendChild(weightInput)
-
-        label = document.createElement("label")
-        label.innerText = "Reps:"
-        label.setAttribute("for","smith-shoulder-press-set1-reps")
-        smithShoulderPressContainerSet1.appendChild(label)
-
-        let repsInput = document.createElement("input")
-        repsInput.setAttribute("type","number")
-        repsInput.setAttribute("step","1")
-        repsInput.setAttribute("id","smith-shoulder-press-set1-reps")
-        repsInput.setAttribute("name","smith-shoulder-press-set1-reps")
-        smithShoulderPressContainerSet1.appendChild(repsInput)
-
-        // set 2
-        container = document.createElement("div")
-        container.setAttribute("id","smith-shoulder-press-set2-container")
-        smithShoulderPressContainer.appendChild(container)
-
-        let smithShoulderPressContainerSet2 = document.getElementById("smith-shoulder-press-set2-container")
-        h4 = document.createElement("h4")
-        h4.innerText = "Set 2"
-        smithShoulderPressContainerSet2.appendChild(h4)
-
-        label = document.createElement("label")
-        label.innerText = "Weight:"
-        label.setAttribute("for","smith-shoulder-press-set2-weight")
-        smithShoulderPressContainerSet2.appendChild(label)
-
-        weightInput = document.createElement("input")
-        weightInput.setAttribute("type","number")
-        weightInput.setAttribute("step","0.01")
-        weightInput.setAttribute("id","smith-shoulder-press-set2-weight")
-        weightInput.setAttribute("name","smith-shoulder-press-set2-weight")
-        smithShoulderPressContainerSet2.appendChild(weightInput)
-
-        label = document.createElement("label")
-        label.innerText = "Reps:"
-        label.setAttribute("for","smith-shoulder-press-set2-reps")
-        smithShoulderPressContainerSet2.appendChild(label)
-
-        repsInput = document.createElement("input")
-        repsInput.setAttribute("type","number")
-        repsInput.setAttribute("step","1")
-        repsInput.setAttribute("id","smith-shoulder-press-set2-reps")
-        repsInput.setAttribute("name","smith-shoulder-press-set2-reps")
-        smithShoulderPressContainerSet2.appendChild(repsInput)
-
-        // set 2
-        container = document.createElement("div")
-        container.setAttribute("id","smith-shoulder-press-set3-container")
-        smithShoulderPressContainer.appendChild(container)
-
-        let smithShoulderPressContainerSet3 = document.getElementById("smith-shoulder-press-set3-container")
-        h4 = document.createElement("h4")
-        h4.innerText = "Set 3"
-        smithShoulderPressContainerSet3.appendChild(h4)
-
-        label = document.createElement("label")
-        label.innerText = "Weight:"
-        label.setAttribute("for","smith-shoulder-press-set3-weight")
-        smithShoulderPressContainerSet3.appendChild(label)
-
-        weightInput = document.createElement("input")
-        weightInput.setAttribute("type","number")
-        weightInput.setAttribute("step","0.01")
-        weightInput.setAttribute("id","smith-shoulder-press-set3-weight")
-        weightInput.setAttribute("name","smith-shoulder-press-set3-weight")
-        smithShoulderPressContainerSet3.appendChild(weightInput)
-
-        label = document.createElement("label")
-        label.innerText = "Reps:"
-        label.setAttribute("for","smith-shoulder-press-set3-reps")
-        smithShoulderPressContainerSet3.appendChild(label)
-
-        repsInput = document.createElement("input")
-        repsInput.setAttribute("type","number")
-        repsInput.setAttribute("step","1")
-        repsInput.setAttribute("id","smith-shoulder-press-set3-reps")
-        repsInput.setAttribute("name","smith-shoulder-press-set3-reps")
-        smithShoulderPressContainerSet3.appendChild(repsInput)
-
-
-
-
+        thursday.forEach(item=>addExerciseToPage(form,item[0],item[1]))
+        addSubmitButton(form)
     }
     else if(day===5){
-        let container = document.createElement("div")
-        container.setAttribute("id","leg-press-glutes-container")
-        form.appendChild(container)
-
-        let smithHackSmithContainer = document.getElementById("leg-press-glutes-container")
-        let h3 = document.createElement("h3")
-        h3.innerText = "Leg Press (Glutes)"
-        smithHackSmithContainer.appendChild(h3)
+        friday.forEach(item=>addExerciseToPage(form,item[0],item[1]))
+        addSubmitButton(form)
     }
 }
